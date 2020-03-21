b39dfbb54a306e3b11b5bcbd755c103269d35909
yipjie <dev@otdd.io>
2020-03-21 23:52:40 +0800

    update

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@
     <div class="header-item" style="margin-right:100px"></div>
     <div class="header-item"><nuxt-link to="/releases" style="color:#F7F7F7;">Releases</nuxt-link></div>
     <div class="header-item"><nuxt-link to="/getstarted" style="color:#F7F7F7;">Get Started</nuxt-link></div>
-    <div class="header-item"><nuxt-link to="/whyotdd" style="color:#F7F7F7;">How OTDD Works</nuxt-link></div>
+    <div class="header-item"><nuxt-link to="/howotddworks" style="color:#F7F7F7;">How OTDD Works</nuxt-link></div>
     <div class="header-item"><nuxt-link to="/whyotdd" style="color:#F7F7F7;">Why OTDD</nuxt-link></div>
     <div style="clear:both"></div>
   </div>
---
+++ pages/howotddworks.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <Header />
+    <div style="margin:0px 140px;text-align:left;padding-bottom:300px;">
+      <div style="font-size:24px;font-weight:bold;padding:40px 0px 20px 0px;">There are two main problems need to be solved by OTDD</div>
+      <ul style="font-size:14px;padding:10px 20px;">
+         <li style="font-size:18px;">How Test Cases Are Recorded</li>
+         <li style="font-size:18px;">How Test Cases Are Run</li>
+      </ul>
+      <div style="font-size:24px;font-weight:bold;padding:40px 0px 20px 0px;">How Test Cases Are Recorded</div>
+      <div style="font-size:18px;font-weight:bold;padding:20px 0px 20px 0px;">The main goal of OTDD when recording test cases is to correctly group inbound req/resp with its outbound reqs/resps.</div>
+      <div style="font-size:16px;padding:10px 0px;">Traditionally online inbound requests/responses and outbound requests/responses are very easy to collect, like usig a tcpdump, tcpdump or something else, but these inbound/outbound requests/responses are of little use if there are no correct relations between them.</div>
+      <div style="font-size:16px;padding:10px 0px;">To group the inbound and outbound reqs/resps, there are three methods:</div>
+      <ul style="font-size:14px;padding:10px 20px;">
+         <li>By using trace id</li>
+         <li>By using thread id</li>
+         <li>By using time gap</li>
+      </ul>
+      <div style="font-size:24px;padding:10px 0px;">By Using Trace Id</div>
+      <div style="font-size:16px;padding:10px 0px;">This method utilize the trace id within the inbound req/resp and outbound reqs/resps to group them. But it has two main drawbacks:</div>
+      <ul style="font-size:14px;padding:10px 20px;">
+         <li>The trace id needs application layer to take care of the trace id as to make sure all outbound reqs/resps must carrys the id.</li>
+         <li>Not all protocol provide a flexiable way to inject a trace id like a http header. e.g. the redis or mysql protocol.</li>
+      </ul>
+      <div style="font-size:24px;padding:10px 0px;">By Using Thread Id</div>
+      <div style="font-size:16px;padding:10px 0px;">This method utilize the thread id of the inbound req/res and outbound reqs/resps to group them. But it also has two main drawbacks:</div>
+      <ul style="font-size:14px;padding:10px 20px;">
+         <li>Inbound req/resp and outbound reqs/resps are not necessarily resides in the same thread.</li>
+         <li>Hajack code must be written in order to track the thread id.</li>
+      </ul>
+      <div style="font-size:24px;padding:10px 0px;">By Using Time Gap</div>
+      <div style="font-size:16px;padding:10px 0px;">This method manages to arrange the inbound requests so that they get processed one bye one "serially", with a time gap between them. So within the time gap, the inbound req/resp and outbound reqs/resps are naturally grouped. It is the method OTDD choose to use.</div>
+      <div style="font-size:16px;padding:10px 0px;">Here is how OTDD works:</div>
+      <div style="font-size:16px;padding:10px 0px;">When OTDD is applied, two pods are created: the redirector and recorder. The redirector just looks like another normal pod that receive its requests from upstreams, while the recorder will receive no requests except only from the redirector. The redirector redirect the request at an interval, say 1 second, to the recorder, the recorder then groups the inbound req/resp and the outbound reqs/resps using the time gap.</div>
+      <div style="font-size:16px;padding:10px 0px;">This method has the following main advantages:</div>
+      <ul style="font-size:14px;padding:10px 20px;">
+         <li>This method applis to most scenarios that most microservice fit in this modal.</li>
+         <li>Recording happends at the tcp layer, no need for the application layer to do anything.</li>
+         <li>With the help of kubernetes and istio, it makes the deployment very easy with only one command.</li>
+      </ul>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
+export default {
+  components: {
+    Header, Footer
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color:#F7F7F7;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+}
+</stype>
